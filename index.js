b7c18a00a499799de79e3d5ccb6da738d4d9b85e
tobyealden <toby.ealden@gmail.com>
2015-06-29 09:06:54 +0100

    move from SVN

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by toby on 27/06/15.
+ */
+
+"use strict";
+
+(function() {
+  var log = require("debug")("index");
+  var Promise = require("bluebird");
+  var config = require("./config.json");
+  var eventBus = require("./lib/busClient").EventBus;
+
+  var DatasetProjection = require("./lib/projections/datasetProjection").Projection;
+  var IOTHubProjection = require("./lib/projections/iotHubProjection").Projection;
+  var IOTFeedProjection = require("./lib/projections/iotFeedProjection").Projection;
+
+  var datasetProj = new DatasetProjection();
+  var iotHubProj = new IOTHubProjection();
+  var iotFeedProj = new IOTFeedProjection();
+
+  var mongoose = require("mongoose");
+  Promise.promisifyAll(mongoose);
+
+  var db = mongoose.connect(config.db.url).connection;
+  db.on("error", function(err) {
+    log("failed to connect to database at %s [%s]",config.db.url, err.message);
+    throw err;
+  });
+
+  db.once("open", function() {
+    eventBus
+      .then(function() { return iotHubProj.start(); })
+      .then(function() { return iotFeedProj.start(); })
+      .then(function() { return datasetProj.start(); })
+      .catch(function(err) {
+        log("fatal error: %s",err.message);
+        throw err;
+      });
+  });
+
+}());
