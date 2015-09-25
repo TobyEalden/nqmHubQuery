de5dc743052e57b9f707009853e377cabf86f681
tobyealden <toby.ealden@gmail.com>
2015-09-25 17:44:02 +0100

    Integrate with command access tokens

--- index.js
+++ index.js
@@ -7,35 +7,21 @@
 (function() {
   var log = require("debug")("nqm-query:index");
   var errLog = require("debug")("nqm-query:error");
-  var Promise = require("bluebird");
   var config = require("./config.json");
-  var QueryListener = require("./lib/httpQueryListener").Listener;
-  var queryListener = new QueryListener();
-  var authServer = require("./lib/authServer");
-  var readModels = require("nqm-read-model");
-  var _models;
+  var queryListener = new (require("./lib/httpQueryListener").Listener)();
+  var authServer = require("nqm-auth-server");
+  var queryDB = require("nqm-read-model");
 
-  var mongoose = require("mongoose");
-  Promise.promisifyAll(mongoose);
-
-  var db = mongoose.createConnection(config.db.url);
-  db.on("error", function(err) {
-    errLog("failed to connect to database at %s [%s]",config.db.url, err.message);
-    throw err;
-  });
-
-  db.once("open", function() {
-      log("database connected");
-      _models = readModels(db);
-      authServer.start(config.authServer)
-        .then(function() {
-          return queryListener.start(config.httpQueryListener, _models)
-        })
-        .catch(function(err) {
-          errLog("fatal error: %s",err.message);
-          errLog(err.stack);
-          process.exit();
-        });
-  });
-
+  queryDB.createConnection(config.db)
+    .then(function() {
+      return authServer.start(config.authServer, queryDB.db());
+    })
+    .then(function() {
+      return queryListener.start(config.httpQueryListener)
+    })
+    .catch(function(err) {
+      errLog("fatal error: %s",err.message);
+      errLog(err.stack);
+      process.exit();
+    });
 }());
